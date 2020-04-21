ee8962f5d425ee4db7baddf62bc5c0e2c3de6c11
yahooserg <seltexinc@gmail.com>
2020-04-21 11:37:34 +0700

    init base pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    OrdersComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
