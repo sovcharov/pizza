3c75e0deca833e68f587139e35acdde1732157c6
yahooserg <seltexinc@gmail.com>
2020-04-21 12:51:35 +0700

    Home page content added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+
+export class HomeComponent implements OnInit {
+  name = "";
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+
+  public goToMenu() {
+    this.router.navigateByUrl("/menu");
   }
 
 }
