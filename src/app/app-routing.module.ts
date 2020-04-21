7b7f274115deb1b3df217fd213ff5b290429de90
yahooserg <seltexinc@gmail.com>
2020-04-21 11:56:43 +0700

    Basic routes add and test with navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '',
+      redirectTo: 'home',
+      pathMatch: 'full'
+    },
+    { path: 'home', component: HomeComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'orders', component: OrdersComponent },
+    { path: 'cart', component: CartComponent },
+    { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
