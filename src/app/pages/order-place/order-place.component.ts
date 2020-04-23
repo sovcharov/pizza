be111fb8a54e6f8e385741695cb2e31d5792aef5
yahooserg <seltexinc@gmail.com>
2020-04-23 17:32:20 +0700

    Order place add form

--- src/app/pages/order-place/order-place.component.ts
+++ src/app/pages/order-place/order-place.component.ts
@@ -9,8 +9,19 @@ export class OrderPlaceComponent implements OnInit {
 
   constructor() { }
 
+  user = {
+    address: "Mani st 23"
+  }
+
+  phone: string = "";
+
   ngOnInit(): void {
-    
+
+  }
+
+  public testNumber () {
+    let pattern = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/;
+
   }
 
 }
