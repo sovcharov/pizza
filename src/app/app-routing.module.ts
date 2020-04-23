2ccc3c7a107eaf7584e15517ac99c99459c78080
yahooserg <seltexinc@gmail.com>
2020-04-23 16:52:08 +0700

    Order place page init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { OrderPlaceComponent } from './pages/order-place/order-place.component';
+
 
 const routes: Routes = [
     { path: '',
@@ -14,6 +16,7 @@ const routes: Routes = [
     { path: 'menu', component: MenuComponent },
     { path: 'orders', component: OrdersComponent },
     { path: 'cart', component: CartComponent },
+    { path: 'order-place', component: OrderPlaceComponent },
     { path: '**', component: HomeComponent }
 ];
 
